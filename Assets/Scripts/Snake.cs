644a0a7ae8efb88ff4a48b117b6a6a599ad893df
Haoyu Li <lihaoyu2678@gmail.com>
2017-09-26 10:24:44 -0700

    first commit

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake: MonoBehaviour {
+
+	private Snake next;
+	private Vector2 position;
+
+	public void SetNext(Snake nextSnake, Vector2 nextPos)
+	{
+		next = nextSnake;
+		position = nextPos;
+	}
+
+	public Snake GetNext()
+	{
+		return next;
+	}
+
+	public void RemoveTail()
+	{
+		Destroy (this.gameObject);
+	}
+
+}
